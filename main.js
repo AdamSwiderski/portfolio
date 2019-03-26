1e63c1b9f093bb617a5ac30e817f75d10594f7e9
Adam Świderski <adamswiderski10@gmail.com>
2019-03-26 22:26:28 +0100

    change icons

--- main.js
+++ main.js
@@ -8,12 +8,49 @@ const titleHtml = document.querySelector('.hobby-title');
 const images = ['img/programowac.jpg', 'img/zwiedzac.jpg', 'img/sluchac.jpg'];
 const imagesTitle = ['Programować', 'Poznawać Świat', 'Słuchać dobrej muzyki'];
 let activeElement = 0;
+let textStartIndex = 0;
+let textTitleIndex = 0;
+let textSubTitleIndex = 0;
+let activeText= 0;
+let activeLetter = 0;
+
 
 burger.addEventListener("click", function () {
     iconBars.classList.toggle("show");
     iconX.classList.toggle("show");
     nav.classList.toggle("active");
 });
+
+function addStartText(){
+
+    const welcomeStart = document.querySelector('.welcome-start');
+    const welcomeStartText = 'Witaj';
+    welcomeStart.textContent += welcomeStartText[textStartIndex];
+    textStartIndex++;
+    if(textStartIndex === welcomeStartText.length) clearInterval(textStartAnimation); 
+};
+
+function addTitleText(){
+
+    const welcomeTitle = document.querySelector('.welcome-title');
+    const welcomeTitleText = 'nazywam się Adam Świderski';
+    welcomeTitle.textContent += welcomeTitleText[textTitleIndex];
+    textTitleIndex++;
+    if(textTitleIndex === welcomeTitleText.length) clearInterval(textTitleAnimation);
+};
+
+function addSubTitleText(){
+
+    const welcomeSubTitle = document.querySelector('.welcome-sub-title');
+    const welcomeSubTitleText = 'Chciałbym zostać profesjonalnym Front-Endowcem';
+    welcomeSubTitle.textContent += welcomeSubTitleText[textSubTitleIndex];
+    textSubTitleIndex++;
+    if(textSubTitleIndex === welcomeSubTitleText.length) clearInterval(textSubTitleAnimation);
+};
+
+const textStartAnimation = setInterval(addStartText, 150);
+const textTitleAnimation = setInterval(addTitleText, 100);
+const textSubTitleAnimation = setInterval(addSubTitleText, 80);
 
 function changeElement() {
     activeElement++;
@@ -40,3 +77,4 @@ function scroll(e) {
 }
 
 $('a[href^="#"]').click(scroll);
+
